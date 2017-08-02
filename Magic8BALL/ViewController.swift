756994cf417c3d1845299f52f91bd0699e6746c3
Francis Jemuel Bergonia <francisyuweh@Franciss-MacBook-Pro.local>
2017-08-02 22:19:18 +0800

    initial commit

--- Magic8BALL/ViewController.swift
+++ Magic8BALL/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     var ballArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")]
     
-    var randomBallNumber = ballArray
+    var randomBallNumber
     
     @IBOutlet weak var imageView: UIImageView!
     
