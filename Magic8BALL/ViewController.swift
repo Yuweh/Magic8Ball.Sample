4ad68226ad9055116ad9853f2f4664578c3b1f5b
Francis Jemuel Bergonia <francisb.itcareers@gmail.com>
2017-11-27 00:24:25 +0800

    Updated to Swift 4

--- Magic8BALL/ViewController.swift
+++ Magic8BALL/ViewController.swift
@@ -10,27 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var ballArray = [ball1, ball2, ball3,ball4, ball5]
-    
+    var ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
     var randomBallNumber: Int = 0
-    
     @IBOutlet weak var imageView: UIImageView!
     
+    func newBallImage(){
+        randomBallNumber = Int(arc4random_uniform(5))
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        randomBallNumber = Int(arc4random_uniform(3))
-        imageView.image = UIImage(named) randomBallNumber
+        newBallImage()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func askButtonPressed(_ sender: Any) {
         
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        newBallImage()
     }
+
 
 }
-
